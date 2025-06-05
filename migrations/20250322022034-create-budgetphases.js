5a7ff142f93b7be7359f0d4db0271435a8f6d88e
Michael <munatsimike@gmail.com>
2025-06-05 13:09:24 +0200

    allow null on created at

--- migrations/20250322022034-create-budgetphases.js
+++ migrations/20250322022034-create-budgetphases.js
@@ -39,7 +39,7 @@ module.exports = {
       },
       createdAt: {
         type: Sequelize.DATE,
-        allowNull: false,
+        allowNull: true,
         defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
       },
       updatedAt: {
